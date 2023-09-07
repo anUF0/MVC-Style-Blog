f27c2d3de701c4091371b95c89243ee42a221546
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-09-07 11:43:59 +1000

    homepage boilplate added

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const routes = require("./controllers");
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: "Super secret secret",
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -23,10 +23,10 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
