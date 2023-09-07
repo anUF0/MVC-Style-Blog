d5fe729e88fb47af78b502504e8db85389b8654c
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-09-07 11:10:14 +1000

    Models Added

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Post = require('./Post');
+
+User.hasMany(Post,{
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment,{
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment,{
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post,{
+    foreignKey: 'post_id'
+});
